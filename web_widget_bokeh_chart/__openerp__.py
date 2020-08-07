OCA/web

[9.0][ADD] web_widget_bokeh_chart

web_widget_bokeh_chart/__openerp__.py
@@ -1,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).#
+
+{
+    "name": "Web Widget Bokeh Chart",
+    "category": "Hidden",
+    "summary": "This widget allows to display charts using Bokeh library.",
+    "author": "Eficent, "
+          "Odoo Community Association (OCA)",
+    "version": "9.0.1.0.0",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "data": [
+        "views/web_widget_bokeh_chart.xml",
+    ],
+    "external_dependencies": {
+        "python": ['bokeh'],
+    },
+    "auto_install": True,
+    "license": "AGPL-3",
+}

vertelab/odoo-oca-web

web_widget_bokeh_chart/__openerp__.py
@@ -1,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).#
+
+{
+    "name": "Web Widget Bokeh Chart",
+    "category": "Hidden",
+    "summary": "This widget allows to display charts using Bokeh library.",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "version": "9.0.1.0.0",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "data": [
+        "views/web_widget_bokeh_chart.xml",
+    ],
+    "external_dependencies": {
+        "python": ['bokeh'],
+    },
+    "auto_install": False,
+    "license": "AGPL-3",
+}
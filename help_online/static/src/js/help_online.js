96d7755b58fb74665dce7477bcf578ea5fd87ef4
victor ekstrom <victor.ekstrom@vertel.se>
2021-06-15 13:58:42 +0000

    help_online: fixes for odoo14. There were serious issues with the rpc functions, so it now bypasses them entirely in favor of direct jquery

--- help_online/static/src/js/help_online.js
+++ help_online/static/src/js/help_online.js
@@ -8,35 +8,61 @@ odoo.define('oca.HelpOnline', function (require) {
     var ControlPanel = require('web.ControlPanel');
     var Dialog = require('web.Dialog');
     var utils = require('web.utils');
+    const rpc = require("web.rpc");
+    var time = require('web.time');
     
     ControlPanel.patch('oca.HelpOnline.monkeypatch', T =>
         class extends T {
                 _formatFields(fields) {
-                    
+                    var ret = super._formatFields(fields);
                     var self = this;
-                    // Reference hooks
-                   console.log('Nu kör vi!', this);
-                   let view = this.props.view || {};
-                   if ($(self.el).find('div.o_help_online_buttons').length == 0 && view.model) {
-                       this.rpc({model: 'help.online', method: 'get_page_url', args: [view.model, view.type]}).then(function(result) {
-                           console.log(result);
-                            if (result && ! _.isEmpty(result)) {
-                                var $helpButton =  self.render_help_button(result);
+                    let view = this.props.view || {};
+                    if ($(self.el).find('div.o_help_online_buttons').length == 0 && view.model) {
+                        var data = {
+                            "jsonrpc": "2.0",
+                            "method": "call",
+                            "params": {
+                                "args": [
+                                    view.model, view.type
+                                ],
+                                "model": 'help.online',
+                                "method": 'get_page_url',
+                                "kwargs": {
+                                    "context": odoo.session_info.user_context
+                                }
+                            },
+                        //this is how odoo core calculates id. Yes, it is indeed dumb
+                        "id": Math.floor(Math.random() * 1000 * 1000 * 1000)
+                        };
+                        var url = "/web/dataset/call_kw/help.online/get_page_url";
+                        //we have to make a manual jquery call because of odoo core issues with rpc
+                        $.ajax(url, _.extend({}, {}, {
+                            url: url,
+                            dataType: 'json',
+                            type: 'POST',
+                            data: JSON.stringify(data, time.date_to_utc),
+                            contentType: 'application/json'
+                        })).then(function(result) {
+                            console.log(result);
+                            if (result.result && ! _.isEmpty(result.result)) {
+                                var $helpButton =  self.render_help_button(result.result);
                                 // update the control panel with the new help button
                                 console.log($helpButton);
-                                $(self.el).find('nav.o_cp_switch_buttons').after($helpButton);
+                                $(self.el).find('div.o_cp_bottom_right').append($helpButton);
                             }
                         });
                     }
-                   return super._formatFields(fields);
+                   return ret
                 }
-                
+
                 render_help_button(url_info) {
-                    console.log("render_help_button")
+                    console.log("url info: ", url_info);
                     var $helpButton = $(QWeb.render("HelpOnline.Button", {'url_info': url_info}));
                     $helpButton.tooltip();
                     if (url_info.exists === false) {
+                        console.log("im true");
                         $helpButton.on('click', function (event) {
+                            console.log("hej", arguments);
                             var evt = event;
                             evt.preventDefault();
                             var self = this;

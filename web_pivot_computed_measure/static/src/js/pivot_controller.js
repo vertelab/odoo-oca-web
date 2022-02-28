49c5f8fa6958eb0258c8525dce972afd70bea28d
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-02-28 13:32:11 +0000

    Now the computed measure options shows up in the menu, and javascipt no longer breaks when we toggle a computed measure

--- web_pivot_computed_measure/static/src/js/pivot_controller.js
+++ web_pivot_computed_measure/static/src/js/pivot_controller.js
@@ -35,11 +35,8 @@ odoo.define("web_pivot_computed_measure.PivotController", function(require) {
          * @override
          */
         renderButtons: function($node) {
-            console.log("RENDER BUTTON MARCUS OVERIDE")
-            console.log($node)
             this._super.apply(this, arguments);
             //~ if ($node) {
-            console.log("RENDER BUTTON MARCUS IF CASE")
             this._renderComputedMeasures();
             //~ }
         },
@@ -51,20 +48,16 @@ odoo.define("web_pivot_computed_measure.PivotController", function(require) {
          */
         _onButtonClick: function(event) {
             const $target = $(event.target);
-            console.log("_ONBUTTONCLICK MARCUS1")
             if ($target.parents("div[data-id='__computed__']").length) {
                 let hideMenu = false;
                 event.preventDefault();
-                console.log("_ONBUTTONCLICK MARCUS2")
                 if (
                     $target.hasClass("dropdown-item") ||
                     $target.hasClass("o_submenu_switcher")
                 ) {
                     this.computed_measures_open = !this.computed_measures_open;
-                    console.log("_ONBUTTONCLICK MARCUS3")
                     this._renderComputedMeasures();
                 } else if ($target.hasClass("o_add_computed_measure")) {
-                    console.log("_ONBUTTONCLICK MARCUS4")
                     hideMenu = true;
                     const field1 = this.$buttons_measures_ex
                         .find("#computed_measure_field_1")
@@ -76,7 +69,6 @@ odoo.define("web_pivot_computed_measure.PivotController", function(require) {
                         .find("#computed_measure_operation")
                         .val();
                     if (oper === "custom") {
-                        console.log("_ONBUTTONCLICK MARCUS5")
                         oper = this.$buttons_measures_ex
                             .find("#computed_measure_operation_custom")
                             .val();
@@ -106,11 +98,20 @@ odoo.define("web_pivot_computed_measure.PivotController", function(require) {
 
                 return;
             }
-            console.log("_ONBUTTONCLICK MARCUS6")
-            console.log(arguments)
-            console.log(event)
-            this._super.apply(this, arguments);
-            
+         
+            if (event.target.closest('.o_pivot_measures_list')) {
+                event.preventDefault();
+                event.stopPropagation();
+                const field = event.target.dataset.field;
+                if (field) {
+                  this.model._toggleMeasure(field).then(this.update.bind(this, {}, { reload: false }));
+                  //~this.update({ measure: field }); THIS SHIT BREAKS IN 14 
+                }
+            }
+            else
+            {
+                    this._super.apply(this, arguments);
+            }
         },
 
         /**
@@ -119,16 +120,13 @@ odoo.define("web_pivot_computed_measure.PivotController", function(require) {
          * @private
          */
         _renderComputedMeasures: function() {
-            console.log("RENDER !!!!!!!!!!!!!!!!!!")
             console.log(this.$buttons_measures_ex)
-            console.log("RENDER !!!!!!!!!!!!!!!!!!")
             if (this.$buttons_measures_ex && this.$buttons_measures_ex.length) {
                 this.$buttons_measures_ex.remove();
             }
             const measures = _.sortBy(_.pairs(_.omit(this.measures, "__count")), x => {
                 return x[1].string.toLowerCase();
             });
-            console.log("button_measures")
             this.$buttons_measures_ex = $(
                 //~ LOOK HERE !!!!!!!!!!!!!!!!!!!!!!!!!!! Opens the view, doesn't seem to work atm
                 QWeb.render("web_pivot_computed_measure.ExtendedMenu", {
@@ -187,12 +185,6 @@ odoo.define("web_pivot_computed_measure.PivotController", function(require) {
          * @param {ChangeEvent} ev
          */
         _onChangeComputedMeasureOperation: function(ev) {
-            console.log("_onChangeComputedMeasureOperation")
-            console.log("_onChangeComputedMeasureOperation")
-            console.log("_onChangeComputedMeasureOperation")
-            console.log("_onChangeComputedMeasureOperation")
-            console.log("_onChangeComputedMeasureOperation")
-            console.log("_onChangeComputedMeasureOperation")
             const $option = $(ev.target.options[ev.target.selectedIndex]);
             if ($(ev.target).val() === "custom") {
                 this.$buttons_measures_ex
